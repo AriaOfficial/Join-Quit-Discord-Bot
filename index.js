cefb8ea786aadf9d8fc70f34679ae7c841f5b7a5
AriaOfficial <47064320+AriaOfficial@users.noreply.github.com>
2019-03-20 15:37:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = ".";
 
-client.login('NTU3OTA3MTc1NTQxMDQ3Mjk2.D3PH3Q.uzKtQ1v4a4NBs48A1uuKhQYjshs');
+client.login('token');
 
 client.on("ready", () => {
 	console.log("[AriaTeam] > Le bot est bien activer !")
@@ -14,7 +14,7 @@ client.on('guildMemberAdd', member =>{
     let embed = new Discord.RichEmbed()
         .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
         .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('557907866888175616').send(embed)
+    member.guild.channels.get('ChannelJoinId').send(embed)
 });
 
 /* Quit */
@@ -22,6 +22,6 @@ client.on('guildMemberRemove', member =>{
     let embed = new Discord.RichEmbed()
         .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
         .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-    member.guild.channels.get('557907926266937374').send(embed)
+    member.guild.channels.get('ChannelQuitId').send(embed)
  
 });
