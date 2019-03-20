eada2605f60118e0fbe4177cdd83ad5102c9b071
AriaOfficial <47064320+AriaOfficial@users.noreply.github.com>
2019-03-20 15:42:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+/*
+
+   _____ __________.___   _____       ______________________   _____      _____                       
+  /  _  \\______   \   | /  _  \      \__    ___/\_   _____/  /  _  \    /     \                      
+ /  /_\  \|       _/   |/  /_\  \       |    |    |    __)_  /  /_\  \  /  \ /  \                     
+/    |    \    |   \   /    |    \      |    |    |        \/    |    \/    Y    \                    
+\____|__  /____|_  /___\____|__  /      |____|   /_______  /\____|__  /\____|__  /                    
+        \/       \/            \/                        \/         \/         \/                     
+                                                                                                      
+                                                                                                      
+                                                                                                      
+                                                                                                      
+                                                                                                                                                                                                          
+________  .___  __________________  ________ __________________       ________  _______________   ____
+\______ \ |   |/   _____/\_   ___ \ \_____  \\______   \______ \      \______ \ \_   _____/\   \ /   /
+ |    |  \|   |\_____  \ /    \  \/  /   |   \|       _/|    |  \      |    |  \ |    __)_  \   Y   / 
+ |    `   \   |/        \\     \____/    |    \    |   \|    `   \     |    `   \|        \  \     /  
+/_______  /___/_______  / \______  /\_______  /____|_  /_______  /    /_______  /_______  /   \___/   
+        \/            \/         \/         \/       \/        \/             \/        \/            
+
+	-> Founder: JblusDev & SmoilDev
+	-> Discord: Soon
+	-> Website: Soon
+	-> Team: Aria
+	-> Goal: CyberSecurity and development of site, software, application and other
+
+
+*/
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = ".";
@@ -9,7 +38,7 @@ client.on("ready", () => {
 	client.user.setGame("Join & Quit > DiscordBot")
 });
 
-/* JOin */
+/* Join */
 client.on('guildMemberAdd', member =>{
     let embed = new Discord.RichEmbed()
         .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
